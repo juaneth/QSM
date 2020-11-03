5a8c9bc49be44ff409ea9a5f38978a621220e4af
fuvkingxase <68202118+fuvkingxase@users.noreply.github.com>
2020-11-03 19:45:16 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace install.cs
+{
+    class Program
+    {
+        public static string IP { get; private set; }
+
+        //AdbS
+
+        public static String adbS(String Argument)
+        {
+            String User = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            ProcessStartInfo s = new ProcessStartInfo();
+            s.CreateNoWindow = false;
+            s.UseShellExecute = false;
+            s.FileName = "adb.exe";
+            s.WindowStyle = ProcessWindowStyle.Minimized;
+            s.RedirectStandardOutput = true;
+            s.Arguments = Argument;
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(s))
+                {
+                    String IPS = exeProcess.StandardOutput.ReadToEnd();
+                    exeProcess.WaitForExit();
+                    return IPS;
+                }
+            }
+            catch
+            {
+
+                ProcessStartInfo se = new ProcessStartInfo();
+                se.CreateNoWindow = false;
+                se.UseShellExecute = false;
+                se.FileName = User + "\\AppData\\Roaming\\SideQuest\\platform-tools\\adb.exe";
+                se.WindowStyle = ProcessWindowStyle.Minimized;
+                se.RedirectStandardOutput = true;
+                se.Arguments = Argument;
+                try
+                {
+                    // Start the process with the info we specified.
+                    // Call WaitForExit and then the using statement will close.
+                    using (Process exeProcess = Process.Start(se))
+                    {
+                        String IPS = exeProcess.StandardOutput.ReadToEnd();
+                        exeProcess.WaitForExit();
+                        return IPS;
+
+                    }
+                }
+                catch
+                {
+                    // Log error.
+                    return "Error";
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Get Quest IP
+
+            String IPS = adbS("shell ifconfig wlan0");
+            int Index = IPS.IndexOf("inet addr:");
+            Boolean space = false;
+            String FIP = "";
+            for (int i = 0; i < IPS.Length; i++)
+            {
+                if (i > (Index + 9) && i < (Index + 9 + 16))
+                {
+                    if (IPS.Substring(i, 1) == " ")
+                    {
+                        space = true;
+                    }
+                    if (!space)
+                    {
+                        FIP = FIP + IPS.Substring(i, 1);
+                    }
+                }
+            }
+
+            if (FIP == "")
+            {
+                IP = "Quest IP";
+                return;
+            }
+            IP = FIP;
+
+            using (WebClient client = new WebClient())
+            {
+                //upload file
+                string filename = "auto-debris_v0.1.1.zip";
+                client.UploadFile("http://" + IP + ":50000/host/beatsaber/upload?overwrite", filename);
+                client.QueryString.Add("foo", "foo");
+                int delay = 2000;
+                System.Threading.Thread.Sleep(delay);
+                client.UploadValues("http://" + IP + ":50000/host/beatsaber/commitconfig", "POST", client.QueryString);
+            }
+        }
+    }
+}
+ 
---
+++ QuestModsPage1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QSM
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form1 = new MainMenu();
+            form1.Closed += (s, args) => this.Close();
+            form1.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string qosmeticsinstall = appPath + "/install-qosmetics.cs.exe";
+            string qosmeticsdownload = "https://github.com/RedBrumbler/Qosmetics/releases/latest/download/Qosmetics.zip";
+
+            if (!File.Exists(appPath + "/qosmetics.zip"))
+            {
+                Process.Start(qosmeticsdownload);
+
+
+                int delay = 3000;
+                System.Threading.Thread.Sleep(delay);
+
+                if (File.Exists(appPath + "/qosmetics.zip"))
+                {
+                    Process.Start(qosmeticsinstall);
+                }
+            }
+
+            else Process.Start(qosmeticsinstall);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string CFTinstall = appPath + "/install-customfailtext.cs.exe";
+            string CFTdownload = "https://cdn.discordapp.com/attachments/765630505000894484/771818619515699230/custom-fail-text_v0.1.6.zip";
+
+            if (!File.Exists(appPath + "/custom-fail-text_v0.1.6.zip"))
+            {
+                Process.Start(CFTdownload);
+
+                int delay = 3000;
+                System.Threading.Thread.Sleep(delay);
+
+                if (File.Exists(appPath + "/custom-fail-text_v0.1.6.zip"))
+                {
+                    Process.Start(CFTinstall);
+                }
+             
+            }
+
+            else Process.Start(CFTinstall);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string ADinstall = appPath + "/install-autodebris.cs.exe";
+            string ADdownload = "https://cdn.discordapp.com/attachments/765630505000894484/771810848959496192/auto-debris_v0.1.1.zip";
+
+            if (!File.Exists(appPath + "/auto-debris_v0.1.1.zip"))
+            {
+                Process.Start(ADdownload);
+
+                int delay = 3000;
+                System.Threading.Thread.Sleep(delay);
+
+                if (File.Exists(appPath + "/auto-debris_v0.1.1.zip"))
+                {
+                    Process.Start(ADinstall);
+                }
+
+            }
+
+            else Process.Start(ADinstall);
+        }
+    }
+}
