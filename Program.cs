97975ef676a0a1e6ef02e21be8c65b3f5a87ad0e
fuvkingxase <68202118+fuvkingxase@users.noreply.github.com>
2020-11-05 16:38:52 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using QSM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace testt
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new QSM.MainMenu());
+        }
+    }
+}
